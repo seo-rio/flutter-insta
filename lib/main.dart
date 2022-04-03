36b510cd8af15a366b542485f8c386b607a3c044
seo-rio <scs9310@gmail.com>
2022-04-03 23:43:21 +0900

    theme data 적용법 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
+import './style.dart';
 
 void main() {
   runApp(
       MaterialApp(
         // Web에 전역 CSS 같은거 (다른 파일로 뺄 수 있음)
-        theme: ThemeData(
-          iconTheme: IconThemeData(
-            color: Colors.blue
-          ),
-          appBarTheme: AppBarTheme(
-            color: Colors.grey,
-            // Deep 한 위젯은 새롭게 다시 설정해줘야할때가 있음
-            actionsIconTheme: IconThemeData(color: Colors.red)
-          ),
-          textTheme: TextTheme(
-            bodyText2: TextStyle(color: Colors.red),
-          ),
-        ),
+        theme: theme,
         home: MyApp()
       )
   );
@@ -30,9 +19,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [Icon(Icons.star)]),
-      // body: Icon(Icons.star),
-      body: Text('안녕'),
+      appBar: AppBar(
+        title: Text('Instagram'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: (){},
+            iconSize: 30,
+          ),
+        ]),
+      // ThemeData 찾아서 textTheme.bodyText2의 Style을 적용해줌
+      body: Text('안녕', style: Theme.of(context).textTheme.bodyText2),
+      // Container 자식부터는 새로운 style을 적용하고 싶을때
+      // body: Theme(
+      //   data: ThemeData(
+      //     textTheme: TextTheme()
+      //   ),
+      //   child: Container(),
+      // ),
     );
   }
 }
