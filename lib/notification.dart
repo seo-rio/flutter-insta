769521edf41fd8c2c1fa8f304fa5a642fffc0571
seo-rio <scs9310@gmail.com>
2022-04-10 22:28:04 +0900

    notification 기능 추가

---
+++ lib/notification.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+final notifications = FlutterLocalNotificationsPlugin();
+
+
+//1. 앱로드시 실행할 기본설정
+initNotification(BuildContext context) async {
+
+  //안드로이드용 아이콘파일 이름
+  var androidSetting = AndroidInitializationSettings('app_icon');
+
+  //ios에서 앱 로드시 유저에게 권한요청하려면
+  var iosSetting = IOSInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  var initializationSettings = InitializationSettings(
+      android: androidSetting,
+      iOS: iosSetting
+  );
+  await notifications.initialize(
+    initializationSettings,
+    //알림 누를때 함수실행하고 싶으면
+    onSelectNotification: (payload) {
+      Navigator.push((context), MaterialPageRoute(builder: (context) => Text('새로운 페이지')));
+      // Navigator.push((context), MaterialPageRoute(builder: (context) => Text(payload!)));
+    }
+  );
+}
+
+//2. 이 함수 원하는 곳에서 실행하면 알림 뜸
+showNotification() async {
+
+  var androidDetails = AndroidNotificationDetails(
+    '유니크한 알림 채널 ID',
+    '알림종류 설명',
+    priority: Priority.high,
+    importance: Importance.max,
+    color: Color.fromARGB(255, 255, 0, 0),
+  );
+
+  var iosDetails = IOSNotificationDetails(
+    presentAlert: true,
+    presentBadge: true,
+    presentSound: true,
+  );
+
+  // 알림 id, 제목, 내용 맘대로 채우기
+  notifications.show(
+    1,
+    '제목1',
+    '내용1',
+    NotificationDetails(android: androidDetails, iOS: iosDetails),
+    payload: '부가정보'
+  );
+}
+
+showNotification2() async {
+
+  // 시간 관련 함수를 쓰려면 써야함
+  tz.initializeTimeZones();
+
+  var androidDetails = const AndroidNotificationDetails(
+    '유니크한 알림 ID',
+    '알림종류 설명',
+    priority: Priority.high,
+    importance: Importance.max,
+    color: Color.fromARGB(255, 255, 0, 0),
+  );
+  var iosDetails = const IOSNotificationDetails(
+    presentAlert: true,
+    presentBadge: true,
+    presentSound: true,
+  );
+
+  notifications.zonedSchedule(
+    2,
+    '제목2',
+    '내용2',
+    makeDate(8, 30, 0), // 8시 30분 00초
+    // tz.TZDateTime.now(tz.local).add(Duration(seconds: 5)), // 알림을 띄울 원하는 시간 입력 (현재 시간 5초 뒤에 알림 띄움)
+    NotificationDetails(android: androidDetails, iOS: iosDetails),
+    androidAllowWhileIdle: true,
+    uiLocalNotificationDateInterpretation:
+    UILocalNotificationDateInterpretation.absoluteTime,
+    matchDateTimeComponents: DateTimeComponents.time // 매일 같은 시간에 알림 띄워줌
+    // matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime // 매주 같은 시간에 알림 띄워줌
+  );
+}
+
+makeDate(hour, min, sec){
+  var now = tz.TZDateTime.now(tz.local);
+  var when = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, min, sec);
+  if (when.isBefore(now)) {
+    return when.add(Duration(days: 1));
+  } else {
+    return when;
+  }
+}
